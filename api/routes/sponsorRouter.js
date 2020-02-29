d199ca9a7adcde871602233571ab9b3012088bc1
igndeldel <58361976+igndeldel@users.noreply.github.com>
2020-02-29 19:54:01 +0100

    CRUD methods

--- api/routes/sponsorRouter.js
+++ api/routes/sponsorRouter.js
@@ -1,16 +1,14 @@
 'use strict';
 
 module.exports = function (app) {
-    var sponsor = require('../controllers/sponsorController'); //Solo se muestra si ha pagado
-
-    app.route('/v1/sponsor/search')
-        .get(sponsor.search_a_sponsor);
-
-    app.route('/v1/sponsor/:sponsorId')
-        .put(sponsor.update_a_sponsor)
-        .delete(sponsor.delete_a_sponsor);
+    var sponsor = require('../controllers/sponsorController'); 
 
     app.route('/v1/sponsor')
-        .post(sponsor.create_a_sponsor);
+        .get(sponsor.list_all_sponsorships)
+        .post(sponsor.create_a_sponsorship)
 
+    app.route('/v1/sponsor/:sponsorId')
+        .get(sponsor.read_a_sponsorship)
+        .put(sponsor.update_a_sponsorship)
+        .delete(sponsor.delete_a_sponsorship)
 };
