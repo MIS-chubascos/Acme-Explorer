714028df2496e49af981d4339cbb2b721d3af5f8
igndeldel <58361976+igndeldel@users.noreply.github.com>
2020-02-29 20:03:40 +0100

    CRUD methods

--- api/controllers/sponsorController.js
+++ api/controllers/sponsorController.js
@@ -1,76 +1,67 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    SponsorShip = mongoose.model('Sponsor');
-
-exports.search_a_sponsor = function (req, res) {
-
-    var actor_id = req.query.actorId;
+    Patrocinio = mongoose.model('Sponsorships');
 
 
-    var aggregate_body = [];
-    if (typeof actor_id !== "undefined") {
-        aggregate_body = [{ $match: { sponsor: mongoose.Types.ObjectId(actor_id) } }];
-    }
-
-    SponsorShip.aggregate(aggregate_body, function (err, sponsor) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(sponsor);
+exports.create_a_sponsorship=function(req,res){
+    var new_sponsorship = new Patrocinio(req.body);
+    new_sponsorship.save(function(err,sponsorship){
+        if(err){
+            if(err.name=='ValidationError'){
+                res.status(422).send(err);
+            }else{
+                res.status(500).send(err);
+            }
+        }else{
+            res.json(sponsorship);
         }
     });
+}
 
-};
-
-exports.update_a_sponsor = function (req, res) {
-    Sponsor.findById(req.params.sponsorId, function (err, actor) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            Sponsor.findOneAndUpdate({ _id: req.params.sponsorId }, req.body, {
-                new: true,
-                upsert: true,
-                setDefaultsOnInsert: true,
-                runValidators: true,
-                context: 'query'
-            }, 
-            function (err, actor) {
-                if (err) {
-                    res.send(err);
-                }
-                else {
-                    res.json(actor);
-                }
-            });
+exports.read_a_sponsorship=function(req,res){
+    Patrocinio.findById(req.params.sponsorshipId, function(req,res){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json(sponsorship);
         }
     });
-};
+}
 
-exports.delete_a_sponsor = function (req, res) {
-    Sponsor.remove({ _id: req.params.sponsorId }, function (err, sponsor) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.json({ 
-                message: 'Sponsor eliminado correctamente' 
-            });
+exports.delete_a_sponsorship=function(req,res){
+    Patrocinio.deleteOne(req.params.sponsorshipId, function(req,sponsorship){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json({message:'Sponsorship Deleted!'});
         }
     });
-};
-exports.create_a_sponsor = function (req, res) {
-    //Compruena si el usuario es un sponsor y, si no, devuelve 'res.status(403): 
-    //Acceso válido pero requiere más privilegios'
+}
 
-    var new_sponsor = new Sponsor(req.body);
-    new_sponsor.save(function (err, sponsor) {
-        if (err) {
-            res.send("403 ERROR - "+ err);
-        }
-        else {
-            res.json(sponsor);
+exports.update_a_sponsorship=function(req,res){
+    Patrocinio.findOneAndUpdate({_id: req.params.sponsorshipId}, req.body, {new:true}, function(err,sponsorship){
+        if(err){
+            if(err.name=='ValidationError'){
+                res.status(422).send(err);
+            }else{
+            res.status(500).send(err);
+            }   
+        }else{
+            res.json(sponsorship);
         }
     });
-};
+}
+
+
+exports.list_all_sponsorships=function(req,res){
+    Patrocinio.find(function(err,Sponsor){
+
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.json(sponsorship);
+        }
+    });
+}
+
