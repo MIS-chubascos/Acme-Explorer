d7be7c56be139d7da3f5a4239b55862e3763428f
David <david.posada.mena@gmail.com>
2020-02-29 10:39:20 +0100

    Update actorController.js
    Changed the update method
    Added ban/unban methods

--- api/controllers/actorController.js
+++ api/controllers/actorController.js
@@ -17,52 +17,6 @@ exports.listAllActors = function (req, res) {
     });
 };
 
-exports.loginAnActor = async function (req, res) {
-    console.log('starting login');
-    var emailParam = req.query.email;
-    var password = req.query.password;
-    Actor.findOne({ email: emailParam }, function (err, actor) {
-        if (err) { res.send(err); }
-
-        // Wrong email provided
-        else if (!actor) {
-            res.status(401); //access token not provided or invalid
-            res.json({ message: 'forbidden', error: err });
-        }
-
-        else if (actor.banned == true) {
-            res.status(403); //access token valid. User is banned
-            res.json({ message: 'forbidden', error: err });
-        }
-        else {
-            // Check the pWord
-            //console.log('actorController pWord: '+password);
-            actor.verifyPassword(password, async function (err, isMatch) {
-                if (err) {
-                    res.send(err);
-                }
-
-                // Password mismatch
-                else if (!isMatch) {
-                    res.status(401); //access token not provided or invalid
-                    res.json({ message: 'forbidden', error: err });
-                }
-
-                else {
-                    try {
-                        var customToken = await admin.auth().createCustomToken(actor.email);
-                    } catch (error) {
-                        console.log("Error creating custom token:", error);
-                    }
-                    actor.customToken = customToken;
-                    console.log('Login Success... sending JSON with custom token');
-                    res.json(actor);
-                }
-            });
-        }
-    });
-};
-
 //Should be an admin
 exports.createAnActor = function (req, res) {
     var new_actor = new Actor(req.body);
@@ -87,22 +41,27 @@ exports.readAnActor = function (req, res) {
     });
 };
 
-//Should be an admin (?) himself
+//This method updates the profile. For banning an actor, use ban/unban
+//Email can't be updated considering that is the account's loggin method (?)
 exports.updateAnActor = function (req, res) {
     Actor.findById(req.params.actorId, function (err, actor) {
         if (err) {
-            res.send(err);
+            res.status(500).send(err);
         } else {
-            var new_actor = req.body;
-            Actor.findOneAndUpdate({ _id: req.params.actorId }, req.body, {
-                new: true,
-                upsert: true,
-                setDefaultsOnInsert: true,
-                runValidators: true,
-                context: 'query'
+            Actor.findOneAndUpdate({ _id: req.params.actorId }, 
+                {name: req.body.name, 
+                    surname: req.body.surname, 
+                    phoneNumber: req.body.phoneNumber, 
+                    address: req.body.address, 
+                    password: req.body.password},
+                {new: true,
+                    upsert: true,
+                    setDefaultsOnInsert: true,
+                    runValidators: true,
+                    context: 'query'
             }, function (err, actor) {
                 if (err) {
-                    res.send(err);
+                    res.status(500).send(err);
                 }
                 else {
                     res.json(actor);
@@ -111,6 +70,36 @@ exports.updateAnActor = function (req, res) {
         }
     });
 };
+
+// Ban/unban methods. Only for admin users
+
+exports.banActor = function(req,res){
+    //If not and admin, res.status(403); "More privileges required due to this action"
+    Actor.findOneAndUpdate({ _id: req.params.actorId},
+        { $set: {"banned": "true"}},
+            {new: true},
+            function (err,actor){
+                if (err){
+                    res.status(500).send(err);
+                }else{
+                    res.json.send(actor);
+                }
+            })
+}
+
+exports.unbanActor = function(req,res){
+    //If not and admin, res.status(403); "More privileges required due to this action"
+    Actor.findOneAndUpdate({ _id: req.params.actorId},
+        { $set: {"banned": "false"}},
+            {new: true},
+            function (err,actor){
+                if (err){
+                    res.status(500).send(err);
+                }else{
+                    res.json.send(actor);
+                }
+            })
+}
 
 exports.validateAnActor = function (req, res) {
     //Check Admin. If not -> res.status(403); "valid access token. Required higher privileges"
@@ -160,6 +149,55 @@ exports.getTripApplicationsByActor = function(req, res) {
 
         } else {
             res.send(tripApplications);
+        }
+    });
+};
+
+
+/** Log methods. We will use firebase (has not yet been taught in class) */
+
+exports.loginAnActor = async function (req, res) {
+    console.log('starting login');
+    var emailParam = req.query.email;
+    var password = req.query.password;
+    Actor.findOne({ email: emailParam }, function (err, actor) {
+        if (err) { res.send(err); }
+
+        // Wrong email provided
+        else if (!actor) {
+            res.status(401); //access token not provided or invalid
+            res.json({ message: 'forbidden', error: err });
+        }
+
+        else if (actor.banned == true) {
+            res.status(403); //access token valid. User is banned
+            res.json({ message: 'forbidden', error: err });
+        }
+        else {
+            // Check the pWord
+            //console.log('actorController pWord: '+password);
+            actor.verifyPassword(password, async function (err, isMatch) {
+                if (err) {
+                    res.send(err);
+                }
+
+                // Password mismatch
+                else if (!isMatch) {
+                    res.status(401); //access token not provided or invalid
+                    res.json({ message: 'forbidden', error: err });
+                }
+
+                else {
+                    try {
+                        var customToken = await admin.auth().createCustomToken(actor.email);
+                    } catch (error) {
+                        console.log("Error creating custom token:", error);
+                    }
+                    actor.customToken = customToken;
+                    console.log('Login Success... sending JSON with custom token');
+                    res.json(actor);
+                }
+            });
         }
     });
 };
