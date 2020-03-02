8cf7dfd63f3118d90396e05f0c86a5996ae25107
David <david.posada.mena@gmail.com>
2020-03-02 01:55:37 +0100

    Update actorController.js
    Cube building

--- api/controllers/actorController.js
+++ api/controllers/actorController.js
@@ -151,10 +151,106 @@ exports.getTripApplicationsByActor = function(req, res) {
     });
 };
 
-/** Cube */
-exports.cubeFunction = function({explorer, period},res){
+/** Cube */  
+
+/**Launch a process to compute a cube of the form M[e, p] that returns the amount of
+money that explorer e has spent on trips during period p, which can be M01-M36 to
+denote any of the last 1-36 months or Y01-Y03 to denote any of the last three years. */
+
+/**We will receive two parameters from a form: 
+ * An explorer
+ * An ammount of time for making a period [Today -> 1 to 36 months]. It can be given in years (1 to 3)
+*/
+
+/**According to the requirements, it can be introduced as M01-M36 or as Y01-Y03. 
+ * We need to parse the string and upercase it for evading minor typing errors
+ */
+function getPeriod(string){
+    try{ 
+        var initDate = new Date(); //The first day of the period (setted below)
+        var uppercased  = string.toUpperCase(); //Upercased input
+        var numbers = Number(uppercased.substring(1,3));  //Syntax for the form M01-M36
+        
+        //Now we have two posibilities Mxx or Yxx
+        //First months (M)
+        if(string.startsWith("M")){
+            if(numbers<1||numbers>36){ //Months can be 1 to 36
+                return{error: "Please insert a month between 1 and 36 both included"}; 
+            }else{ //
+                if(numbers<12){ //less than 1 year
+                    initDate.setMonth(initDate.getMonth()-numbers);
+                }else if(numbers>11 && numbers<24){ //1 year
+                    initDate.setFullYear((initDate.getFullYear-1), (initDate.getMonth()-(numbers-12)));
+                }else if(numbers>23 && numbers<37){ // 2 year
+                    initDate.setFullYear((initDate.getFullYear-2), (initDate.getMonth()-(numbers-24)));
+                }
+            }
+        //now years (Y)
+        }else if(string.startsWith("Y")){
+            if(numbers<1||numbers>3){
+                return{error: "Please insert a year between 1 and 3 both included"};
+            }else{
+                initDate.setFullYear(initDate.getFullYear()-numbers)
+            }
+        }else{  //bad syntax
+            return{error:"Period must be as following: M01-M36 of Y01-Y03"}
+        } 
+        return{initDate: initDate}
+    } catch(error){ //Probably bad syntax
+    return{error:"Period must be as following: M01-M36 of Y01-Y03"}
+    }
+}
+
+//Once we have processed the date we can 
+exports.cubeData = function(req,res){
+    var endDate = new Date(); //Today, the last day of the period
+    var explorer = req.body.explorer; //We receive an explorer from a form
+    var periodPreProcesed = req.body.period;  //We receive the period input
+    var period = getPeriod(periodPreProcesed); //Process the period for obtaining two dates 
+
+    if(period.error){
+        res.status(400).send(period.error);
+    }else{
+        var initialDay = period.initDate; //Date for making the period
+    
+        var trips = TripApplication.aggregate([
+            {
+                $match: {   //filtering the trips of the explorer by his applications
+                    status:"ACCEPTED",
+                    explorer: explorer,
+                    moment: {
+                        $gte: initialDay,
+                        $lte: endDate
+                    }
+                }
+            }, {
+                $project: {  //we need the trips for calculating the total ammount of money
+                    _id:false,
+                    trips: "$trip"
+                }
+            }
+        ], function(err,res){
+            if(err){
+                res.send(err);
+            }else{
+                res(res[0]); //if all is ok, return the necesary data 
+            }
+        })
+    }
+}
+
+// Calculate things (here or avobe (?) )
+exports.cube=function(req,res){
 
 }
+
+
+
+
+
+
+
+
 
 /** Log methods. We will use firebase (has not yet been taught in class) */
 
