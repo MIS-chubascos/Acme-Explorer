5c6b97ca81125b2c6cd236476b8191076f88619c
David <david.posada.mena@gmail.com>
2020-03-02 15:37:52 +0100

    Update actorRoutes.js
    Added the cube methods routes

--- api/routes/actorRoutes.js
+++ api/routes/actorRoutes.js
@@ -17,4 +17,12 @@ module.exports = function (app) {
     
     app.route('/actors/:actorId/tripApplications')
         .get(actors.getTripApplicationsByActor);
-};
+
+    //given an explorer and a period, return the result of query. check 'sumPrice' atribute 
+    app.route('/actors/cubeDataMoney/:explorer/:period')
+        .get(actors.cubeDataMoney)
+
+    // get all M[p,e] > V    need ask period, ammount and comparator 
+    app.route('/actors/cubeDataComparator/:period/:ammount/:comparator')
+        .get(actors.cubeDataComparator)
+    };
