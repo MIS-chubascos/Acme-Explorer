404884f2821da458ab773447aa8d700abcd63e42
igndeldel <58361976+igndeldel@users.noreply.github.com>
2020-03-16 00:43:53 +0100

    Created tests for sponsorships

---
+++ test/sponsorships.specs.js
@@ -0,0 +1,47 @@
+const app = require("../app");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+var mongoose = require('mongoose'),
+  Trip = mongoose.model('Sponsorship');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+describe("Trip API Testing", () => {
+  it("Get Sponsorships", done => {
+    chai
+      .request(app)
+      .get("/api/v1/sponsorships")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect('Content-Type', /json/);
+        if (err) done(err);
+        else done();
+      });
+  });
+//sponsor, trip y banner?
+ it("Post Sponsorship", done => {
+    var trip = {"_id": "test_sponsorship", "sponsor":"123456", "url":"www.terra.es","banner":"123456","trip":"123456","payed":"True"}
+    chai
+      .request(app)
+      .post("/api/v1/trips")
+      .send(trip)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        if (err) done(err);
+        else done();
+      });
+  });
+
+  it("Update Sponsorship", (done) => {
+    var trip = {"url":"www.terra.es","banner":"123456","trip":"123456","payed":"True"}
+    chai
+      .request(app)
+      .put("/api/v1/sponsorships/test_trip")
+      .send(sponsorship)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        if (err) done(err);
+        else done();
+      });
+  });
+});
