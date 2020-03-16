ca7fa6960a29628eda2cb35f88b0947539c33347
Alicia <aliviaord@alum.us.es>
2020-03-16 13:00:37 +0100

    V2 methods for Finder 🌃

--- api/routes/finderRoutes.js
+++ api/routes/finderRoutes.js
@@ -2,12 +2,24 @@
 
 module.exports = function(app) {
     var finders = require('../controllers/finderController');
+    var authController = require('../controllers/authController');
+    const V1_API_PATH = '/api/v1';
+    const V2_API_PATH = '/api/v2';
 
-    app.route('/finders/:finderId')
+    // V1 methods
+    app.route(V1_API_PATH + '/finders/:finderId')
         .get(finders.getFinder)
         .put(finders.updateFinder);
 
-    app.route('/finders/:finderId/trips')
+    app.route(V1_API_PATH + '/finders/:finderId/trips')
         .get(finders.getTripsByFinder);
 
+    // V2 methods
+    app.route(V2_API_PATH + '/finders/:finderId')
+        .get(authController.verifyUser(['EXPLORER']), finders.getFinder)
+        .put(authController.verifyUser(['EXPLORER']), finders.updateFinder);
+
+    app.route(V2_API_PATH + '/finders/:finderId/trips')
+        .get(authController.verifyUser(['EXPLORER']), finders.getTripsByFinder);
+    
 }
